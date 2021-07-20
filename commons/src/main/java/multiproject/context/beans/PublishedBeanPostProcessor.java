6c72ba577f85d08efc388d2c5e171dcb85b08d00
smoothbear <kjbin0420@naver.com>
2021-07-20 21:22:58 +0900

    Add PublishedBeanPostProcessor

--- commons/src/main/java/multiproject/context/beans/PublishedBeanPostProcessor.java
+++ commons/src/main/java/multiproject/context/beans/PublishedBeanPostProcessor.java
@@ -2,8 +2,19 @@ package multiproject.context.beans;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public class PublishedBeanPostProcessor implements BeanPostProcessor {
+    private static final Class<Published> publishedAnnotation = Published.class;
+
+    private final ConfigurableListableBeanFactory beanFactory;
+    // private final ConfigurableListableBeanFactory parentBeanFactory;
+
+    public PublishedBeanPostProcessor(ConfigurableListableBeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+        // this.parentBeanFactory =
+    }
+
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
